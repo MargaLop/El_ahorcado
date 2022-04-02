3094e811b70ad7158ec95d917b40ed2d1f7a5602
Marga <margalppalacios@gmail.com>
2022-04-02 12:49:13 +0200

    lista de palabras + bienvenida

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Ejercico de pruba. Un ejercicio simulando al juego clasico "el ahorcadp".
 
+Basado en el proyecto de: Kylie Ying (@kylieyying). 
+Versión en Español con Modificaciones: Estefania Cassingena Navone (@EstefaniaCassN).
---
+++ main.py
@@ -0,0 +1,13 @@
+def ahorcado ():
+    nombre_usuario = input('¿Cuál es su nombre?:')
+    print (f'''
+     .-.     .-.     .-.     .-.     .-.     .-.     .-.
+`._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
+         Bienvenido {nombre_usuario} al Ahorcado
+     .-.     .-.     .-.     .-.     .-.     .-.     .-.
+`._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
+''')
+   
+ahorcado()
+
+
