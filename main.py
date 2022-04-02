ec156996eb72de47e1fab528bc67d5257a1c1946
Marga <margalppalacios@gmail.com>
2022-04-02 13:30:27 +0200

    comienzo

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
-def ahorcado ():
+import random
+import string
+from doc_palabras import lista_palabras
+
+
+
+
+
+def palabra_azar_ordenador(palabras):
+        return random.choice(palabras)  #el programa escoge una palabra al azar de nuestro archivo doc_palabras
+
+
+def ahorcado (palabras):
+      
+    palabra = palabra_azar_ordenador(palabras)
+
+    vidas = 7
+    while  vidas > 0:
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
     nombre_usuario = input('¿Cuál es su nombre?:')
     print (f'''
      .-.     .-.     .-.     .-.     .-.     .-.     .-.
@@ -7,7 +47,69 @@ def ahorcado ():
      .-.     .-.     .-.     .-.     .-.     .-.     .-.
 `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
 ''')
+    ahorcado()
    
-ahorcado()
+ #asci dibujos
+
+'''
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+ =========
+
+   +---+
+   |   |
+   O   |
+       |
+       |
+       |
+ =========
+
+   +---+
+   |   |
+   O   |
+   |   |
+       |
+       |
+ =========
+
+   +---+
+   |   |
+   O   |
+  /|   |
+       |
+       |
+ =========
+
+   +---+
+   |   |
+   O   |
+  /|\  |
+       |
+       |
+ =========
+
+   +---+
+   |   |
+   O   |
+  /|\  |
+  /    |
+       |
+ =========
 
 
+  +---+
+
+   |   |
+
+   O   |
+
+  /|\  |
+
+  / \  |
+
+       |
+========='''
