c7b4fd1b31990831983985f0b12b273ccf23b040
Marga <margalppalacios@gmail.com>
2022-04-09 12:26:48 +0200

    modulo, dibujos de pinguino

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 ## EL AHORACADO
 
-Ejercico de pruba. Un ejercicio simulando al juego clasico "el ahorcadp".
+Ejercicio de prueba. Un ejercicio simulando al juego clasico "el ahorcado".
 
 Basado en el proyecto de: Kylie Ying (@kylieyying). 
 Versión en Español con Modificaciones: Estefania Cassingena Navone (@EstefaniaCassN).
+
+Este ejercicio se didvide en cuatro modulos.
+    1# "main" : El codigo principal, en el que se desarrolla la base del juego.
+
+    2# "doc_palabras": En este modulo, nos encontramos una lista de palabras, con la cual el codigo de manera aleatoria escoge la palabra usada a lo largo de la partida. Estas palabraslas obtuve de "@EstefaniaCassN".
+
+    3# "vidas_represemtacion_grafica" : En este archivo nos encontramos un diccionario con los clasicos monigotes del ahorcado. Estos dibujos los obtuve de la pagina " https://inventwithpython.com/es/9.html"ç
+
+    4# "dibujos_pinguino" : En el interior de est archivo nos encontramos, al narrador de la historia un pingüino, dando más simpatia al juego, podemos encontrarnos sus distintas interacciones con el usuario.
+
+
--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import string
 import time
 from doc_palabras import lista_palabras
 import vidas_representación_grafica
-
-
-
+import dibujos_pinguino as dp
 
 
 #el programa escoge una palabra al azar de nuestro archivo doc_palabras
@@ -34,42 +32,14 @@ def ahorcado (palabra):
   vidas = 7
 
   while len(letras_palabra) > 0 and  vidas > 0:
-    # print(palabra)
+    print(palabra)
 
     grafica_palabra = progreso_palabra(letras_adivinar, palabra)
-
-
-    print(f'''
-    
-         .-.     .-.     .-.     .-.     .-.     .-.     .-.
-    `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
-
-                              __________________________________________
-                             /                                          \\
-                            |    TUS VIDAS SON[{vidas}]                        |
-                            |                                            |
-             . --- .        |   'PALABRA: {' '.join(grafica_palabra)}")    
-           /        \\        ___________________________________________/                          
-          |  O  _  O |      /
-          |  ./   \. |
-          /  `-._.-'  \\
-        .' /         \ `. ''')
-
+    print(dp.vidas_palabra(vidas,grafica_palabra))
     time.sleep(1)
-    letra_ronda = input(''' 
-  
-                                ________________________________________
-                            /                                           \\
-             . --- .        |             ESCOGE UNA LETRA               |   
-           /        \\        ___________________________________________/                          
-          |  O  _  O |      /
-          |  ./   \. |
-          /  `-._.-'  \\
-        .' /         \ `. 
-        
-    TU LETRA:'''). lower() #es necesario que sean minusculas
-
+    letra_ronda = input(dp.letra_usuario()). lower() #es necesario que sean minusculas
     print(f'''LETRAS ESCOGIDAS:{letra_ronda} ''')
+
 
     if letra_ronda in  letras_abcdario - letras_adivinar : #En caso de que el usuario introduzca caracteres distintos al abc
 
@@ -80,175 +50,28 @@ def ahorcado (palabra):
 
       else:
         vidas -= 1
-        print(f'''
-                               _________________________________________________________
-                             /                                                           \\
-                            |    ESTA LETRA NO ESTA EN LA PALABRA. ME VAS A MATAR        |
-                            |                                                            |
-                            |    {vidas_representación_grafica.repre_vida[vidas]}                                                      
-             . --- .        |                                                            |
-           /  \   /  \\        __________________________________________________________/                          
-          |  O  _  O |      /
-          |  ./   \. |
-          /  `-._.-'  \\
-        .' /         \ `. 
-        ''')
+        print(dp.letra_incorrecta(vidas_representación_grafica.repre_vida[vidas]))
         time.sleep(1)
 
     elif letra_ronda in letras_adivinar:
-      print(f'''
-                                _________________________________________
-                             /                                            \\
-                            |   ESTA LETRA YA FUE ESCOGIDA ANTERIORMENTE  |
-                            |   LAS LETRAS QUE YA ESCOGISTE SON:          |
-             . --- .        |   {letras_adivinar}                          
-           /  \   /  \\        ___________________________________________/                          
-          |  O  _  O |      /
-          |  ./   \. |
-          /  `-._.-'  \\
-        .' /         \ `. ''')
+      print(dp.ya_escogida(letras_adivinar))
       time.sleep(1)
 
     else:
-
-      print(f'''
-
-                             _________________________________________
-                           /                                          \\
-           . --- .         |   ESTO NO ES UNA LETRAAAAAA!!!!!!!!       |                
-         /  \   /  \\        __________________________________________/                          
-        |  O  _  O |      /
-        |  ./   \. |
-        /  `-._.-'  \\
-      .' /         \ `. ''')
-    
+      print(dp.no_letra())
       time.sleep(1)
 
-
-
   if vidas == 0:
-      print(f'''
-             .-.     .-.     .-.     .-.     .-.     .-.     .-.
-        `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
-
-                              ____________________
-                            /                      \\
-                            |    POR TU CULPA,     |
-                            |   LLEGO EL DIA DE MI |
-                            |        MUERTE        |
-                            |    La palabra era    |
-             . --- .        |    {palabra}
-           /        \\        _____________________/                          
-          |  ()  ()  |     /
-          |     ^    |
-          /   |||||  \\       
-        .' /  |||||  \ `.        
-    .-~.-~/           \~-.~-.      
-.-~ ~    |             |    ~ ~-. 
-`- .     |             |     . -'  
-     ~ - |             | - ~     
-         \             /             
-       ___\           /___         
-       ~;_  >- . . -<  _i~                       
-
-                                  
-             .-.     .-.     .-.     .-.     .-.     .-.     .-.
-        `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
-        ''')
-    
+    print(dp.ganar_perder(palabra,False))
+  
   else:
-      print(f'''
-             .-.     .-.     .-.     .-.     .-.     .-.     .-.
-        `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
-                              ____________________
-                            /                      \\
-                            |    ¡¡ENHORABUENA!!   |
-                            |    La palabra era    |
-             . --- .        |    {palabra}
-           /        \\        _____________________/                          
-          |  O  _  O |      /
-          |  ./   \. |
-          /  `-._.-'  \\            .-=========-.
-        .' /         \ `.          \\'-=======-'/
-    .-~.-~/           \~-.~-.      _|   .=.   |_
-.-~ ~    |             |    ~ ~-. ((|  ((1))  |))
-`- .     |             |     . -'  \|   /|\   |/
-     ~ - |             | - ~        \__ '`' __/
-         \             /              _`) (`_
-       ___\           /___          _/_______\_
-       ~;_  >- . . -<  _i~         /___________\\             
-             .-.     .-.     .-.     .-.     .-.     .-.     .-.
-        `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
-
-      ''')
-     
-
-
-
-
-
-
-
-
-
+    print(dp.ganar_perder(palabra,True))
+    
+  
 
 if __name__ == '__main__':
-    nombre_usuario = input('''
-                              ____________________
-                            /                      \\
-             . --- .        | ¿Cuál es su nombre?: |   
-           /        \\        _____________________/                          
-          |  O  _  O |      /
-          |  ./   \. |
-          /  `-._.-'  \\
-        .' /         \ `.
-    .-~.-~/           \~-.~-.
-.-~ ~    |             |    ~ ~-.
-`- .     |             |     . -'
-     ~ - |             | - ~
-         \             /
-       ___\           /___
-       ~;_  >- . . -<  _i~
-       
-
-    NOMBRE:   
-    ''')
-
-
-    print (f'''
-
-     .-.     .-.     .-.     .-.     .-.     .-.     .-.
-`._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'
-                              ____________________
-                            /                      \\
-                            |      Bienvenido      |
-                            |        {nombre_usuario} 
-             . --- .        |      al Ahorcado     |   
-           /        \\        _____________________/                          
-          |  O  _  O |      /
-          |  ./   \. |
-          /  `-._.-'  \\
-        .' /         \ `.
-    .-~.-~/           \~-.~-.
-.-~ ~    |             |    ~ ~-.
-`- .     |             |     . -'
-     ~ - |             | - ~
-         \             /
-       ___\           /___
-       ~;_  >- . . -<  _i~
-       
-''')
-time.sleep(1)
+  nombre_usuario = input(dp.nombre_usuario()).upper()
+  print(dp.bienvenido(nombre_usuario))
+  time.sleep(1)
    
 ahorcado(palabra_azar_ordenador(lista_palabras))
-   
-
-         
-        
-
-
-
-
-
-
-  
